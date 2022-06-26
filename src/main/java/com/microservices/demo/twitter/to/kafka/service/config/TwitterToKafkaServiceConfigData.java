f810eb13895a0630a1a02b28cb2f18671e1ac7f6
Amlan002 <mahapatra.amlan11@gmail.com>
2022-06-26 14:39:34 +0530

    first commit

---
+++ src/main/java/com/microservices/demo/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
@@ -0,0 +1,19 @@
+package com.microservices.demo.twitter.to.kafka.service.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties("twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfigData {
+	private List<String> twitterKeywords;
+	private String msgBody;
+	private String twitterV2BaseUrl;
+	private String twitterV2RulesBaseUrl;
+	private String twitterV2BearerToken;
+}
